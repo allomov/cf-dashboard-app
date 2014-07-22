4a667851906ac5bb48a53c35534368507b4f5c70
Alexander Lomov <lomov.as@gmail.com>
2014-07-22 16:14:30 +0300

    update configuration class

--- lib/cloud_foundry_manager.rb
+++ lib/cloud_foundry_manager.rb
@@ -1,28 +1,29 @@
 require 'configuration'
 
-Config = Configuration.new(File.join(Rails.root, 'config', 'cf.yml'))
-
 class CloudFoundryManager
 
+  def self.config
+    @config ||= Configuration.new('config/cf.yml')
+  end
+
   def self.client
-    puts({username: Config.username, password: Config.password}.inspect)
     @default_client ||= begin 
-      CFoundry::Client.get(Config.api).tap do |client| 
-        client.login(username: Config.username, password: Config.password)
-        client.base.rest_client.log = STDOUT if Config.profile
+      CFoundry::Client.get(config.api).tap do |client| 
+        client.login(username: config.username, password: config.password)
+        client.base.rest_client.log = STDOUT if config.profile
       end
     end
   end
 
-  def self.space(name = Config.space)
-    self.organization.spaces.find {|space| space.name == name}
-  end
-  
-  def self.organization(name = Config.organization)
+  def self.organization(name = config.organization)
     self.client.organizations.find {|org| org.name == name}
   end
 
-  def self.application(name = Config.application)
+  def self.space(name = config.space)
+    self.organization.spaces.find {|space| space.name == name}
+  end
+  
+  def self.application(name = config.application)
     self.space.apps.find {|app| app.name == name}
   end
 
