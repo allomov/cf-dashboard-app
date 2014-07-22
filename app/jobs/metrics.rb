574300f82ab51e5710c8307f8a5bc0c1881daf7f
Alexander Lomov <lomov.as@gmail.com>
2014-07-22 13:31:56 +0300

    move redis config to environment variables

--- app/jobs/metrics.rb
+++ app/jobs/metrics.rb
@@ -15,20 +15,21 @@ Dashing.scheduler.every '1s', allow_overlapping: false do
   stats = app.stats.values
   usage = stats.map { |s| s.has_key?(:stats) ? s[:stats][:usage] : nil }.compact
 
-  mem_usage = usage.map { |u| u[:mem] }
-  cpu_usage = usage.map { |u| u[:cpu] }
-  
-  mem_usage_average = (mem_usage.sum / mem_usage.count) / 1048576   # mb
-  cpu_usage_average = ((cpu_usage.sum / cpu_usage.count) * 100).round(3)       # persents
-  
-  time = time + 1
+  unless usage.empty?
+    mem_usage = usage.map { |u| u[:mem] }
+    cpu_usage = usage.map { |u| u[:cpu] }
+    
+    mem_usage_average = (mem_usage.sum / mem_usage.count) / 1048576   # mb
+    cpu_usage_average = ((cpu_usage.sum / cpu_usage.count) * 100).round(3)       # persents
+    
+    time = time + 1
 
-  cpu_usage_history << {'x' => time, 'y' => cpu_usage_average}
-  mem_usage_history << {'x' => time, 'y' => mem_usage_average}
+    cpu_usage_history << {'x' => time, 'y' => cpu_usage_average}
+    mem_usage_history << {'x' => time, 'y' => mem_usage_average}
 
-  Dashing.send_event('cpu-meter', {value: cpu_usage_average})
-  Dashing.send_event('mem-meter', {value: mem_usage_average})
-  Dashing.send_event('cpu-average', points: cpu_usage_history, displayValue: "#{cpu_usage_average}%")
-  Dashing.send_event('mem-average', points: mem_usage_history, displayValue: "#{mem_usage_average}Mb")
-  
+    Dashing.send_event('cpu-meter', {value: cpu_usage_average})
+    Dashing.send_event('mem-meter', {value: mem_usage_average})
+    Dashing.send_event('cpu-average', points: cpu_usage_history, displayValue: "#{cpu_usage_average}%")
+    Dashing.send_event('mem-average', points: mem_usage_history, displayValue: "#{mem_usage_average}Mb")
+  end
 end
