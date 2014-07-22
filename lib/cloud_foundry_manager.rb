cf955220b5aee792e3bf7bb28fb199c6b49b8e8d
Alexander Lomov <lomov.as@gmail.com>
2014-07-22 13:00:09 +0300

    fix configuration set up

--- lib/cloud_foundry_manager.rb
+++ lib/cloud_foundry_manager.rb
@@ -1,27 +1,28 @@
 require 'configuration'
 
-config = Configuration.new('config/cf.yml')
+Config = Configuration.new(File.join(Rails.root, 'config', 'cf.yml'))
 
 class CloudFoundryManager
 
   def self.client
+    puts({username: Config.username, password: Config.password}.inspect)
     @default_client ||= begin 
-      CFoundry::Client.get(config.api).tap do |client| 
-        client.login(username: config.username, password: config.password)
-        client.base.rest_client.log = STDOUT if config.cf_profile
+      CFoundry::Client.get(Config.api).tap do |client| 
+        client.login(username: Config.username, password: Config.password)
+        client.base.rest_client.log = STDOUT if Config.profile
       end
     end
   end
 
-  def self.space(name = config.space)
+  def self.space(name = Config.space)
     self.organization.spaces.find {|space| space.name == name}
   end
   
-  def self.organization(name = config.org)
+  def self.organization(name = Config.organization)
     self.client.organizations.find {|org| org.name == name}
   end
 
-  def self.application(name = config.app)
+  def self.application(name = Config.application)
     self.space.apps.find {|app| app.name == name}
   end
 
