9ab74c926b7a5b96234bb4e4e8044c1b2741c131
A.S. Lomoff <lomov.as@gmail.com>
2014-08-12 15:37:24 +0300

    ttt

--- app/assets/javascripts/dashing/filters/humanizers.coffee
+++ app/assets/javascripts/dashing/filters/humanizers.coffee
@@ -42,7 +42,7 @@ Batman.Filters.humanize_percents = (value) ->
 
 Batman.Filters.humanize_dea_host = (value) ->
   hosts =
-    '192.168.114.21': 'Pivotal CF, Dea 1'
-    '192.168.114.21': 'Pivotal CF, Dea 1'
+    '192.168.114.21': 'Local CF, Dea 1'
+    '192.168.109.22': 'vCHS CF, Dea 1'
   if hosts[value]? then hosts[value] else value
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,18 +20,18 @@ Rails.application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  # config.serve_static_assets = false
 
   # Compress JavaScripts and CSS.
-  # config.assets.js_compressor = :uglifier
+  # config.serve_static_assets = false
   # config.assets.js_compressor = Uglifier.new(mangle: false)
+  # config.assets.compile = false
   # config.assets.css_compressor = :sass
+  # config.assets.digest = true
+  config.assets.debug = true  
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  # config.assets.compile = false
 
   # Generate digests for assets URLs.
-  # config.assets.digest = true
 
   # `config.assets.precompile` has moved to config/initializers/assets.rb
 
@@ -59,7 +59,7 @@ Rails.application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  config.assets.debug = true
+
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
